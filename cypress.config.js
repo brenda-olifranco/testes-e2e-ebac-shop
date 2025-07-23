214adaefed8050d1a7741b1faa3e1751842f89bf
Brenda Franco <brenda_olifranco@hotmail.com>
2025-07-23 20:31:06 -0300

    ajuste cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,8 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: 'http://localhost:3000',
-    viewportWidth: 1000,
-    viewportHeight: 660
-  }
-});
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+  },
+  reporter: 'mochawesome'
+})
